6e46f901c3fffd323756cd8cb73b5b8a0c7c5825
simedi152006-ux <simedi152006@gmail.com>
2026-09-10 13:25:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import streamlit as st
 from PIL import Image
 from googletrans import Translator
+from streamlit_lottie import st_lottie
+import json
 
 st.title('Análisis de Sentimiento')
 image = Image.open('emoticones.jpg')
@@ -35,7 +37,12 @@ with st.expander('Analizar texto'):
         x=round(blob.sentiment.polarity,2)
         if x > 0.0 and x <=1.0:
             st.write( 'Es un sentimiento Positivo 😊')
+          with open('Happy SUN.json') as source:
+            animation=json.load(source)
+          st.lottie(animation, width=350)
         elif x >=-1 and x <= 0:
             st.write( 'Es un sentimiento Negativo 😔')
         else:
             st.write( 'Es un sentimiento Neutral 😐')
+
+
