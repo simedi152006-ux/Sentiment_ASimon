00ac48de2f95d2ba80c0160fb95c931b3db63289
simedi152006-ux <simedi152006@gmail.com>
2026-09-10 13:41:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -52,19 +52,19 @@ with st.expander('Analizar texto'):
         if x > 0.0 and x <= 1.0:
             st.write('Es un sentimiento Positivo 😊')
             with open('HappySUN.json') as source:
-                animation = json.load(source)
+                animation=json.load(source)
             st.lottie(animation, width=350)
 
         elif x >= -1 and x <= 0:
             st.write('Es un sentimiento Negativo 😔')
             with open('HappySUN.json') as source:
-                animation = json.load(source)
+                animation=json.load(source)
             st.lottie(animation, width=350)
             
         else:
             st.write('Es un sentimiento Neutral 😐')
             with open('HappySUN.json') as source:
-                animation = json.load(source)
+                animation=json.load(source)
             st.lottie(animation, width=350)
 
 
